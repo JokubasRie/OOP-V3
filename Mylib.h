caa413263ddd6dd722f92e4a7be3bdc5a6bb9dd5
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-03 23:03:50 +0300

    Sukuriamas Mylib.h, kuriame surašytos naudotos bibliotekos

---
+++ Mylib.h
@@ -0,0 +1,23 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <random>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+#endif // MYLIB_H_INCLUDED
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+
+
+#endif // MYLIB_H_INCLUDED
