166230d3ae2c8b7189941fa0381752b94da3eb5d
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-03 23:07:06 +0300

    Sukuriamas functions.h, kuriame sudėtos naudotos funkcijos

---
+++ functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#include <vector>
+#include <string>
+#include "student.h"
+
+bool skaitytiDuomenisIsFailo(vector<Student>& students, const string& failoPavadinimas);
+
+void skaitytiDuomenisIsVartotojo(vector<Student>& students);
+
+void spausdintiStudentus(const vector<Student>& students);
+void spausdintiStudentusVidurki(const vector<Student>& students);
+void spausdintiStudentusMediana(const vector<Student>& students);
+
+int atsitiktiniaiBalai(int min = 0, int max = 10);
+void generuotiAtsitiktiniusBalus(Student& student, int kiekNd);
+
+string pasirinktiFaila();
+
+#endif // FUNCTIONS_H_INCLUDED
