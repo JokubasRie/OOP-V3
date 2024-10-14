6cb087b0cfeb68277253f08c3e1a4f27887f8b62
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-14 18:43:56 +0300

    pridedamos naujai sudarytos funkcijos

--- functions.h
+++ functions.h
@@ -20,4 +20,10 @@ void generuotiAtsitiktiniusBalus(Student& student, int kiekNd);
 
 string pasirinktiFaila();
 
+void generuotiStudentuFaila(int studentuSkaicius, const string& failoPavadinimas);
+void rusiuotiStudentus(const vector<Student>& studentai, vector<Student>& vargsiukai, vector<Student>& kietiakiai);
+void spausdintiStudentusIFaila(const vector<Student>& studentai, const string& failoPavadinimas);
+void rusiuotiStudentusIrIssaugotiIFailus(const vector<Student>& studentai);
+void laikuMatavimai(int studentuSkaicius, const std::string& failoPavadinimas);
+void apdorotiVisusFailus();
 #endif // FUNCTIONS_H_INCLUDED
