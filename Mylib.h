c5d11545c040f77c6945500931eb0f2eec3c2c5d
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-14 18:57:37 +0300

    pridedamos papildomos bibliotekos, reikalingos funkcijų veikimui

--- Mylib.h
+++ Mylib.h
@@ -11,8 +11,10 @@
 #include <random>
 #include <numeric>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 #endif // MYLIB_H_INCLUDED
 #ifndef MYLIB_H_INCLUDED
