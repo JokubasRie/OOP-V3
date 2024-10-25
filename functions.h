4c74460754ebb96d891019f1354ed03d6388e16d
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-25 23:00:26 +0300

    atnaujinamas visų funkcijų deklaravimas

--- functions.h
+++ functions.h
@@ -1,26 +1,25 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
-#include <vector>
+#include <list>
 #include <string>
 #include "student.h"
+
 bool skaitytiDuomenisIsFailo(std::list<Student>& students, const std::string& failoPavadinimas);
 void skaitytiDuomenisIsVartotojo(std::list<Student>& students);
-
 void spausdintiStudentus(const std::list<Student>& students);
 void spausdintiStudentusVidurki(const std::list<Student>& students);
 void spausdintiStudentusMediana(const std::list<Student>& students);
 
 bool compareByName(const Student& a, const Student& b);
 int atsitiktiniaiBalai(int min = 0, int max = 10);
-string pasirinktiFaila();
-void generuotiStudentuFaila(int studentuSkaicius, const string& failoPavadinimas);
+std::string pasirinktiFaila();
+void generuotiStudentuFaila(int studentuSkaicius, const std::string& failoPavadinimas);
 void rusiuotiStudentus(const std::list<Student>& studentai, std::list<Student>& vargsiukai, std::list<Student>& kietiakiai);
 void spausdintiStudentusIFaila(const std::list<Student>& studentai, const std::string& failoPavadinimas);
 void rusiuotiStudentusIrIssaugotiIFailus(std::list<Student>& studentai);
 void laikuMatavimai(int studentuSkaicius, const std::string& failoPavadinimas);
 void apdorotiVisusFailus();
-
 void laikuMatavimaiBeGeneravimo(const std::string& failoPavadinimas, char sortChoice);
 
 bool compareBySurname(const Student& a, const Student& b);
