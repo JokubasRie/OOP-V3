9ce72c0f3311a3d5942bd9739008094c0544e644
Jokūbas Riebžda <jokubas.ri@gmail.com>
2024-10-25 20:56:54 +0300

    atnaujinamos laiku skaiciavimo funkcijos

--- functions.cpp
+++ functions.cpp
@@ -286,99 +286,93 @@ void rusiuotiStudentusIrIssaugotiIFailus(vector<Student>& studentai) {
 }
 
 void laikuMatavimaiBeGeneravimo(const string& failoPavadinimas, char sortChoice) {
-    auto readStart = high_resolution_clock::now();
+    ChronoTimer timer;
 
     vector<Student> studentai;
     if (!skaitytiDuomenisIsFailo(studentai, failoPavadinimas)) {
         cerr << "Nepavyko nuskaityti duomenu is failo." << endl;
         return;
     }
+    cout << "Failo nuskaitymo laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto readEnd = high_resolution_clock::now();
-    duration<double> readTime = readEnd - readStart;
-    cout << "Failo nuskaitymo laikas: " << readTime.count() << " s\n";
-
-    auto sortStart = high_resolution_clock::now();
+    timer.reset();
     if (sortChoice == '1') {
         sort(studentai.begin(), studentai.end(), compareByName);
+        cout << "Rusiavimo pagal varda laikas: " << timer.getElapsedTime() << " s\n";
     } else if (sortChoice == '2') {
         sort(studentai.begin(), studentai.end(), compareBySurname);
+        cout << "Rusiavimo pagal pavarde laikas: " << timer.getElapsedTime() << " s\n";
     } else if (sortChoice == '3') {
         sort(studentai.begin(), studentai.end(), compareByGrade);
+        cout << "Rusiavimo pagal galutini pazymi laikas: " << timer.getElapsedTime() << " s\n";
     } else {
         cerr << "Neteisingas pasirinkimas!" << endl;
+        return;
     }
-    auto sortEnd = high_resolution_clock::now();
-    duration<double> sortTime = sortEnd - sortStart;
-    cout << "Rusiavimo laikas: " << sortTime.count() << " s\n";
 
-    auto divideStart = high_resolution_clock::now();
+    timer.reset();
     vector<Student> vargsiukai, kietiakiai;
     rusiuotiStudentus(studentai, vargsiukai, kietiakiai);
-    auto divideEnd = high_resolution_clock::now();
-    duration<double> divideTime = divideEnd - divideStart;
-    cout << "Dalijimo i kietiakus ir vargsiukus laikas: " << divideTime.count() << " s\n";
+    cout << "Dalijimo i kietiakus ir vargsiukus laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto saveVargsiukaiStart = high_resolution_clock::now();
+    timer.reset();
     spausdintiStudentusIFaila(vargsiukai, "vargsiukai.txt");
-    auto saveVargsiukaiEnd = high_resolution_clock::now();
-    duration<double> saveVargsiukaiTime = saveVargsiukaiEnd - saveVargsiukaiStart;
-    cout << "Vargsiuku isvedimo i faila laikas: " << saveVargsiukaiTime.count() << " s\n";
+    cout << "Vargsiuku isvedimo i faila laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto saveKietiakiaiStart = high_resolution_clock::now();
+    timer.reset();
     spausdintiStudentusIFaila(kietiakiai, "kietiakiai.txt");
-    auto saveKietiakiaiEnd = high_resolution_clock::now();
-    duration<double> saveKietiakiaiTime = saveKietiakiaiEnd - saveKietiakiaiStart;
-    cout << "Kietiaku isvedimo i faila laikas: " << saveKietiakiaiTime.count() << " s\n";
+    cout << "Kietiaku isvedimo i faila laikas: " << timer.getElapsedTime() << " s\n";
 }
+
 
 
 
 void laikuMatavimai(int studentuSkaicius, const string& failoPavadinimas) {
-    auto start = high_resolution_clock::now();
+    char sortChoice;
+    cout << "Pagal ka norite rusiuoti? 1 - pagal varda, 2 - pagal pavarde, 3 - pagal galutini pazymi: ";
+    cin >> sortChoice;
 
+    ChronoTimer timer;
     generuotiStudentuFaila(studentuSkaicius, failoPavadinimas);
-
-    auto end = high_resolution_clock::now();
-    duration<double> generationTime = end - start;
     cout << "Sugeneruotas failas: " << failoPavadinimas << " su " << studentuSkaicius << " irasu.\n";
-    cout << "Failo generavimo laikas: " << generationTime.count() << " s\n";
+    cout << "Failo generavimo laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto readStart = high_resolution_clock::now();
+    timer.reset();
     vector<Student> studentai;
     skaitytiDuomenisIsFailo(studentai, failoPavadinimas);
+    cout << "Failo nuskaitymo laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto readEnd = high_resolution_clock::now();
-    duration<double> readTime = readEnd - readStart;
-    cout << "Failo nuskaitymo laikas: " << readTime.count() << " s\n";
+    timer.reset();
+    if (sortChoice == '1') {
+        sort(studentai.begin(), studentai.end(), compareByName);
+        cout << "Rusiavimo pagal varda laikas: " << timer.getElapsedTime() << " s\n";
+    } else if (sortChoice == '2') {
+        sort(studentai.begin(), studentai.end(), compareBySurname);
+        cout << "Rusiavimo pagal pavarde laikas: " << timer.getElapsedTime() << " s\n";
+    } else if (sortChoice == '3') {
+        sort(studentai.begin(), studentai.end(), compareByGrade);
+        cout << "Rusiavimo pagal galutini pazymi laikas: " << timer.getElapsedTime() << " s\n";
+    } else {
+        cerr << "Neteisingas pasirinkimas!" << endl;
+        return;
+    }
 
-    auto sortStart = high_resolution_clock::now();
-    sort(studentai.begin(), studentai.end(), compareByName);
-    auto sortEnd = high_resolution_clock::now();
-    duration<double> sortTime = sortEnd - sortStart;
-    cout << "Rusiavimo pagal varda laikas: " << sortTime.count() << " s\n";
-
-    auto divideStart = high_resolution_clock::now();
+    timer.reset();
     vector<Student> vargsiukai, kietiakiai;
     rusiuotiStudentus(studentai, vargsiukai, kietiakiai);
-    auto divideEnd = high_resolution_clock::now();
-    duration<double> divideTime = divideEnd - divideStart;
-    cout << "Dalijimo i kietiakus ir vargsiukus laikas: " << divideTime.count() << " s\n";
+    cout << "Dalijimo i kietiakus ir vargsiukus laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto saveVargsiukaiStart = high_resolution_clock::now();
+    timer.reset();
     spausdintiStudentusIFaila(vargsiukai, "vargsiukai.txt");
-    auto saveVargsiukaiEnd = high_resolution_clock::now();
-    duration<double> saveVargsiukaiTime = saveVargsiukaiEnd - saveVargsiukaiStart;
-    cout << "Vargsiuku isvedimo i faila laikas: " << saveVargsiukaiTime.count() << " s\n";
+    cout << "Vargsiuku isvedimo i faila laikas: " << timer.getElapsedTime() << " s\n";
 
-    auto saveKietiakiaiStart = high_resolution_clock::now();
+    timer.reset();
     spausdintiStudentusIFaila(kietiakiai, "kietiakiai.txt");
-    auto saveKietiakiaiEnd = high_resolution_clock::now();
-    duration<double> saveKietiakiaiTime = saveKietiakiaiEnd - saveKietiakiaiStart;
-    cout << "Kietiaku isvedimo i faila laikas: " << saveKietiakiaiTime.count() << " s\n";
-    cout << endl;
-
+    cout << "Kietiaku isvedimo i faila laikas: " << timer.getElapsedTime() << " s\n";
 }
+
+
+
 
 void apdorotiVisusFailus() {
     vector<int> studentCounts = {1000, 10000, 100000, 1000000, 10000000};
